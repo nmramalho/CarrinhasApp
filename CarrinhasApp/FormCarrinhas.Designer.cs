88281bd1ae26c428d20a1b0291369e6154307304
Nuno Ramalho <Nuno Ramalho>
2017-10-26 21:22:57 +0100

    Add project files.

---
+++ CarrinhasApp/FormCarrinhas.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace CarrinhasApp
+{
+    partial class FormCarrinhas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCarrinhas));
+            this.userControlCarrinha1 = new ClassLibraryCarrinha.UserControlCarrinha();
+            this.groupBoxCarrinha1 = new System.Windows.Forms.GroupBox();
+            this.labelMotoristaCarrinha1 = new System.Windows.Forms.Label();
+            this.labelLugaresReservadosCarrinha1 = new System.Windows.Forms.Label();
+            this.labelLugaresLivresCarrinha1 = new System.Windows.Forms.Label();
+            this.userControlCarrinha2 = new ClassLibraryCarrinha.UserControlCarrinha();
+            this.groupBoxCarrinha2 = new System.Windows.Forms.GroupBox();
+            this.labelMotoristaCarrinha2 = new System.Windows.Forms.Label();
+            this.labelLugaresReservadosCarrinha2 = new System.Windows.Forms.Label();
+            this.labelLugaresLivresCarrinha2 = new System.Windows.Forms.Label();
+            this.groupBoxCarrinha1.SuspendLayout();
+            this.groupBoxCarrinha2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // userControlCarrinha1
+            // 
+            this.userControlCarrinha1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControlCarrinha1.BackgroundImage")));
+            this.userControlCarrinha1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.userControlCarrinha1.Location = new System.Drawing.Point(39, 3);
+            this.userControlCarrinha1.MotoristaAtribuido = false;
+            this.userControlCarrinha1.Name = "userControlCarrinha1";
+            this.userControlCarrinha1.NumLugaresLivres = 6;
+            this.userControlCarrinha1.NumLugaresReservados = 0;
+            this.userControlCarrinha1.Size = new System.Drawing.Size(483, 245);
+            this.userControlCarrinha1.TabIndex = 0;
+            // 
+            // groupBoxCarrinha1
+            // 
+            this.groupBoxCarrinha1.Controls.Add(this.labelMotoristaCarrinha1);
+            this.groupBoxCarrinha1.Controls.Add(this.labelLugaresReservadosCarrinha1);
+            this.groupBoxCarrinha1.Controls.Add(this.labelLugaresLivresCarrinha1);
+            this.groupBoxCarrinha1.Location = new System.Drawing.Point(563, 71);
+            this.groupBoxCarrinha1.Name = "groupBoxCarrinha1";
+            this.groupBoxCarrinha1.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxCarrinha1.TabIndex = 1;
+            this.groupBoxCarrinha1.TabStop = false;
+            this.groupBoxCarrinha1.Text = "Carrinha 1";
+            // 
+            // labelMotoristaCarrinha1
+            // 
+            this.labelMotoristaCarrinha1.AutoSize = true;
+            this.labelMotoristaCarrinha1.Location = new System.Drawing.Point(6, 25);
+            this.labelMotoristaCarrinha1.Name = "labelMotoristaCarrinha1";
+            this.labelMotoristaCarrinha1.Size = new System.Drawing.Size(53, 13);
+            this.labelMotoristaCarrinha1.TabIndex = 2;
+            this.labelMotoristaCarrinha1.Text = "Motorista:";
+            // 
+            // labelLugaresReservadosCarrinha1
+            // 
+            this.labelLugaresReservadosCarrinha1.AutoSize = true;
+            this.labelLugaresReservadosCarrinha1.Location = new System.Drawing.Point(6, 70);
+            this.labelLugaresReservadosCarrinha1.Name = "labelLugaresReservadosCarrinha1";
+            this.labelLugaresReservadosCarrinha1.Size = new System.Drawing.Size(108, 13);
+            this.labelLugaresReservadosCarrinha1.TabIndex = 1;
+            this.labelLugaresReservadosCarrinha1.Text = "Lugares Reservados:";
+            // 
+            // labelLugaresLivresCarrinha1
+            // 
+            this.labelLugaresLivresCarrinha1.AutoSize = true;
+            this.labelLugaresLivresCarrinha1.Location = new System.Drawing.Point(6, 47);
+            this.labelLugaresLivresCarrinha1.Name = "labelLugaresLivresCarrinha1";
+            this.labelLugaresLivresCarrinha1.Size = new System.Drawing.Size(75, 13);
+            this.labelLugaresLivresCarrinha1.TabIndex = 0;
+            this.labelLugaresLivresCarrinha1.Text = "Lugares livres:";
+            // 
+            // userControlCarrinha2
+            // 
+            this.userControlCarrinha2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("userControlCarrinha2.BackgroundImage")));
+            this.userControlCarrinha2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.userControlCarrinha2.Location = new System.Drawing.Point(40, 236);
+            this.userControlCarrinha2.MotoristaAtribuido = false;
+            this.userControlCarrinha2.Name = "userControlCarrinha2";
+            this.userControlCarrinha2.NumLugaresLivres = 6;
+            this.userControlCarrinha2.NumLugaresReservados = 0;
+            this.userControlCarrinha2.Size = new System.Drawing.Size(482, 260);
+            this.userControlCarrinha2.TabIndex = 2;
+            // 
+            // groupBoxCarrinha2
+            // 
+            this.groupBoxCarrinha2.Controls.Add(this.labelMotoristaCarrinha2);
+            this.groupBoxCarrinha2.Controls.Add(this.labelLugaresReservadosCarrinha2);
+            this.groupBoxCarrinha2.Controls.Add(this.labelLugaresLivresCarrinha2);
+            this.groupBoxCarrinha2.Location = new System.Drawing.Point(563, 306);
+            this.groupBoxCarrinha2.Name = "groupBoxCarrinha2";
+            this.groupBoxCarrinha2.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxCarrinha2.TabIndex = 3;
+            this.groupBoxCarrinha2.TabStop = false;
+            this.groupBoxCarrinha2.Text = "Carrinha 2";
+            // 
+            // labelMotoristaCarrinha2
+            // 
+            this.labelMotoristaCarrinha2.AutoSize = true;
+            this.labelMotoristaCarrinha2.Location = new System.Drawing.Point(6, 25);
+            this.labelMotoristaCarrinha2.Name = "labelMotoristaCarrinha2";
+            this.labelMotoristaCarrinha2.Size = new System.Drawing.Size(53, 13);
+            this.labelMotoristaCarrinha2.TabIndex = 2;
+            this.labelMotoristaCarrinha2.Text = "Motorista:";
+            // 
+            // labelLugaresReservadosCarrinha2
+            // 
+            this.labelLugaresReservadosCarrinha2.AutoSize = true;
+            this.labelLugaresReservadosCarrinha2.Location = new System.Drawing.Point(6, 70);
+            this.labelLugaresReservadosCarrinha2.Name = "labelLugaresReservadosCarrinha2";
+            this.labelLugaresReservadosCarrinha2.Size = new System.Drawing.Size(108, 13);
+            this.labelLugaresReservadosCarrinha2.TabIndex = 1;
+            this.labelLugaresReservadosCarrinha2.Text = "Lugares Reservados:";
+            // 
+            // labelLugaresLivresCarrinha2
+            // 
+            this.labelLugaresLivresCarrinha2.AutoSize = true;
+            this.labelLugaresLivresCarrinha2.Location = new System.Drawing.Point(6, 47);
+            this.labelLugaresLivresCarrinha2.Name = "labelLugaresLivresCarrinha2";
+            this.labelLugaresLivresCarrinha2.Size = new System.Drawing.Size(75, 13);
+            this.labelLugaresLivresCarrinha2.TabIndex = 0;
+            this.labelLugaresLivresCarrinha2.Text = "Lugares livres:";
+            // 
+            // FormCarrinhas
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(796, 535);
+            this.Controls.Add(this.groupBoxCarrinha2);
+            this.Controls.Add(this.userControlCarrinha2);
+            this.Controls.Add(this.groupBoxCarrinha1);
+            this.Controls.Add(this.userControlCarrinha1);
+            this.Name = "FormCarrinhas";
+            this.Text = "Gestão de Carrinhas";
+            this.groupBoxCarrinha1.ResumeLayout(false);
+            this.groupBoxCarrinha1.PerformLayout();
+            this.groupBoxCarrinha2.ResumeLayout(false);
+            this.groupBoxCarrinha2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClassLibraryCarrinha.UserControlCarrinha userControlCarrinha1;
+        private System.Windows.Forms.GroupBox groupBoxCarrinha1;
+        private System.Windows.Forms.Label labelMotoristaCarrinha1;
+        private System.Windows.Forms.Label labelLugaresReservadosCarrinha1;
+        private System.Windows.Forms.Label labelLugaresLivresCarrinha1;
+        private ClassLibraryCarrinha.UserControlCarrinha userControlCarrinha2;
+        private System.Windows.Forms.GroupBox groupBoxCarrinha2;
+        private System.Windows.Forms.Label labelMotoristaCarrinha2;
+        private System.Windows.Forms.Label labelLugaresReservadosCarrinha2;
+        private System.Windows.Forms.Label labelLugaresLivresCarrinha2;
+    }
+}
+
---
+++ CarrinhasApp/FormCarrinhas.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarrinhasApp
+{
+    public partial class FormCarrinhas : Form
+    {
+        public FormCarrinhas()
+        {
+            InitializeComponent();
+
+            this.userControlCarrinha1.BotaoClickHandler(new EventHandler(QuandoBotaoUserControloCarrinha1Pressionado));
+            this.userControlCarrinha2.BotaoClickHandler(new EventHandler(QuandoBotaoUserControloCarrinha2Pressionado));
+        }
+
+        private void QuandoBotaoUserControloCarrinha1Pressionado(object sender, EventArgs e)
+        {
+            string strLivres = userControlCarrinha1.NumLugaresLivres.ToString();
+            string strReservados = userControlCarrinha1.NumLugaresReservados.ToString();
+
+            labelLugaresLivresCarrinha1.Text = "Lugares Livres: " + strLivres;
+            labelLugaresReservadosCarrinha1.Text = "Lugares Reservados: " + strReservados;
+
+
+            if (userControlCarrinha1.MotoristaAtribuido)
+            {
+                labelMotoristaCarrinha1.Text = "Motorista: Sim";    
+            }
+            else
+            {
+                labelMotoristaCarrinha1.Text = "Motorista: Não";
+            }
+            
+        }
+
+
+        private void QuandoBotaoUserControloCarrinha2Pressionado(object sender, EventArgs e)
+        {
+            string strLivres = userControlCarrinha2.NumLugaresLivres.ToString();
+            string strReservados = userControlCarrinha2.NumLugaresReservados.ToString();
+
+            labelLugaresLivresCarrinha2.Text = "Lugares Livres: " + strLivres;
+            labelLugaresReservadosCarrinha2.Text = "Lugares Reservados: " + strReservados;
+
+
+            if (userControlCarrinha2.MotoristaAtribuido)
+            {
+                labelMotoristaCarrinha2.Text = "Motorista: Sim";
+            }
+            else
+            {
+                labelMotoristaCarrinha2.Text = "Motorista: Não";
+            }
+
+        }
+
+
+
+
+    }
+}
